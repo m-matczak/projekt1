50f03ccf1bb1fe58a19c7e449dad8fcace2eae4b
m-matczak <marcin.matczak@student.put.poznan.pl>
2023-12-05 20:59:00 +0000

    Solve FizzBuzz (1)

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ import numpy as np
 
 
 def ex_1():
-    pass
+    for i in range(1,100):
+        string = ""
+        if i % 3 == 0:
+            string = string + "Fizz"
+        if i % 5 == 0:
+            string = string + "Buzz"
+
+        if string == "":
+            print(i)
+            continue
+        else:
+            print(string)
 
 def ex_2():
     pass
