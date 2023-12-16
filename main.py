a578bfeeaa03d8452320e68ec2b582aa80379839
__ <mr.chloor@gmail.com>
2023-12-16 01:56:20 +0100

    Solve 9

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+from datetime import datetime
 import random
 import statistics
-from datetime import datetime
-import csv
+
 
 filepath = "random_numbers"
 
+
 def ex_1():
-    for i in range(1,100):
+    for i in range(1, 100):
         string = ""
         if i % 3 == 0:
             string = string + "Fizz"
@@ -21,13 +23,15 @@ def ex_1():
         else:
             print(string)
 
+
 def ex_2():
     n = int(input("Input how many numbers to generate: "))
 
     with open(filepath, "w") as file:
-        for _ in range(0,n):
-            r = random.randint(1,10)
+        for _ in range(0, n):
+            r = random.randint(1, 10)
             file.write(f"{r}\n")
+
 
 def ex_3():
     lines = []
@@ -35,12 +39,11 @@ def ex_3():
         for line in file.readlines():
             lines.append(int(line))
 
-
     mean = statistics.fmean(lines)
     stdev = -1
     if len(lines) != 1:
         stdev = statistics.stdev(lines)
-    minimum= min(lines)
+    minimum = min(lines)
     maximum = max(lines)
 
     print(f"mean: {mean}")
@@ -48,32 +51,36 @@ def ex_3():
     print(f"minimum: {minimum}")
     print(f"maximum: {maximum}")
 
+
 def ex_4(n):
     if n in {0, 1}:
         return n
     n = ex_4(n-1) + ex_4(n-2)
     return n
 
+
 def ex_5():
     n = int(input("Podaj liczbe n: "))
 
     fib = []
-    for i in range(1,n):
+    for i in range(1, n):
         fib.append(ex_4(i))
 
     plt.plot(fib)
     plt.ylabel("Fibonacci numbers")
     plt.show()
 
+
 def ex_6():
     n = int(input("Podaj liczbe n: "))
 
     d = {}
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         d[i] = i*i
 
     print(d)
     return d
+
 
 def ex_7(d):
 
@@ -82,27 +89,59 @@ def ex_7(d):
         s += v
     print(s)
 
+
 def ex_8():
     n = int(input("Podaj n liczb losowych:"))
-    for fn in range(10):
+    for _ in range(10):
         now = datetime.now()
         filename = now.strftime("%Y-%m-%d_%h-%M-%S-%f")
         with open(filename, "wb") as file:
             random_data = np.random.rand(n)
             file.write(bytearray(random_data))
 
+
+def normalize(x):
+    return np.sqrt(x.dot(x))
+
+
 def ex_9():
-    pass
+    # wykresy
+    data = np.genfromtxt('reference_trajectory.csv', delimiter=',')
+
+    t = data[1:-1, 0]
+    x = data[1:-1, 1]
+    y = data[1:-1, 2]
+    z = data[1:-1, 3]
+
+    fig, axs = plt.subplots(3)
+    fig.suptitle("Pozycja w funkcji czasu")
+
+    axs[0].plot(t, x)
+    axs[1].plot(t, y)
+    axs[2].plot(t, z)
+    plt.show()
+
+    # srednia
+    data = np.genfromtxt('reference_trajectory.csv', delimiter=',')
+    print("Mean values of position:")
+    print(np.mean(data[1:-1, 1:4], axis=0))
+
+    # predkosc
+    velocities = data[1:-1, 4:7]
+    normalized = np.apply_along_axis(normalize, axis=1, arr=velocities)
+    np.savetxt("velocity.csv", normalized)
+
 
 def main():
-    ex_1()
-    ex_2()
-    ex_3()
-    ex_5()
-    d = ex_6()
-    ex_7(d)
-    ex_8()
+    #    ex_1()
+    #    ex_2()
+    #    ex_3()
+    #    ex_5()
+    #    d = ex_6()
+    #    ex_7(d)
+    #    ex_8()
     ex_9()
+
 
 if __name__ == '__main__':
     main()
